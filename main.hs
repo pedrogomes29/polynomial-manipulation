a0b1d927f7bae82b9ac2710f3c435420186bfd8f
Pedro Gomes <pedromgomes29@gmail.com>
2022-10-09 23:09:03 +0100

    compatible with negative coefficients and exponents

--- main.hs
+++ main.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import System.Win32 (COORD(xPos))
 
 type Coef = Int
 type Exponent = Int
@@ -6,9 +7,14 @@ type Var = Char
 type Monomial = (Coef,[(Var,Exponent)])
 type Polynomial = [Monomial]
 
+seperator::Char
+
+isPartOfNumber::Char->Bool
 
 cleanInput:: String -> String
 splitInput:: String -> [String]
+
+addSeperator::String -> String
 
 readVars:: String->[(Var,Exponent)]
 
@@ -19,37 +25,50 @@ polToStr:: Polynomial -> String
 monToStr:: Monomial -> String
 printVars:: [(Var,Exponent)]->String
 
-cleanInput s =  filter (\x -> x/='*' && x/=' ') s
+seperator = '?'
+
+isPartOfNumber c = isDigit c || c=='+' || c=='-'
+
+addSeperator "" = []
+addSeperator (x:xs) |x=='^' = x:(head xs:addSeperator (tail xs))
+                    |x=='-' || x=='+' = seperator:(x:addSeperator xs)
+                    |otherwise = x:addSeperator xs
+
+cleanInput xs     | head cleaned=='-' || head cleaned=='+' = cleaned 
+                  | otherwise = '+':cleaned
+                        where cleaned = filter (\x -> x/='*' && x/=' ') xs
 
 splitInput "" = []
-splitInput s = mon1: splitInput (if mon2=="" then "" else (tail mon2))
-               where (mon1,mon2) = break ('+'==) s
+splitInput s = mon1: splitInput (if mon2=="" then "" else tail mon2)
+               where (mon1,mon2) = break (seperator==) s
 
-
-parseMon s = (read coef,readVars rest)
-            where coef = takeWhile isDigit s
-                  rest = dropWhile isDigit s
 
 readVars "" = []
-readVars (x:[]) = [(x,1)] --if last variable has exponent 1 (ommited)
+readVars [x] = [(x,1)] --if last variable has exponent 1 (ommited)
 readVars s = if head (tail s)=='^' 
-             then (head var, read exponent::Int): readVars otherVars 
+             then (head var, if sign=='-' then -read absExponent else read exponent): readVars otherVars 
              else (head s,1) :readVars (tail s) -- if the next character isn't a carat (^), then the head is a variable with exponent 1 ommited
-                where (var,(carat:rest)) = break('^'==) s  -- carat is the symbol '^'
-                      exponent = takeWhile isDigit rest
-                      otherVars = dropWhile isDigit rest
+                where (var,carat:rest) = break('^'==) s  -- carat is the symbol '^'
+                      exponent = takeWhile isPartOfNumber rest
+                      (sign:absExponent) = exponent
+                      otherVars = dropWhile isPartOfNumber rest
 
-parsePol = map (parseMon) . splitInput . cleanInput
+parsePol = map parseMon . tail . splitInput . addSeperator . cleanInput
 
-monToStr (coef,rest) = show coef ++ printVars rest
+parseMon s = (if sign=='-' then -read absCoef else read absCoef,readVars rest)
+            where (sign:absCoef) = takeWhile isPartOfNumber s
+                  rest = dropWhile isPartOfNumber s
+
+
+monToStr (coef,rest) = (if coef >0 then "+" else "") ++show coef ++ printVars rest
 
 printVars [] = ""
-printVars ((var,exponent):xs)   | exponent == 1 =  [var] ++ printVars xs
+printVars ((var,exponent):xs)   | exponent == 1 =  var: printVars xs
                                 | otherwise = [var] ++ "^" ++ show exponent ++ printVars xs
 
 polToStr [] = ""
-polToStr (x:[]) = monToStr x
-polToStr (x:xs) = monToStr x ++ " + " ++ polToStr xs
+polToStr [x] = monToStr x
+polToStr (x:xs) = monToStr x ++  polToStr xs
                   
 
 
