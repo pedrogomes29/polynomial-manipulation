9bdbee17228603dbb7ddc6c45cb89317c5c8c7b7
Joao <joaorolareis@gmail.com>
2022-10-14 18:14:51 +0100

    Finished sum sub mul and derive

--- main.hs
+++ main.hs
@@ -1,4 +1,6 @@
 import Data.Char
+import Data.List
+
 
 type Coef = Int
 type Exponent = Int
@@ -12,7 +14,6 @@ isPartOfNumber::Char->Bool
 
 cleanInput:: String -> String
 splitInput:: String -> [String]
-
 addSeperator::String -> String
 
 readVars:: String->[(Var,Exponent)]
@@ -23,6 +24,23 @@ parsePol:: String -> Polynomial
 polToStr:: Polynomial -> String
 monToStr:: Monomial -> String
 printVars:: [(Var,Exponent)]->String
+
+sumPol:: Polynomial -> Polynomial -> Polynomial
+subPol:: Polynomial -> Polynomial -> Polynomial
+mulPol:: Polynomial -> Polynomial -> Polynomial
+normPol:: Polynomial -> Polynomial
+derivePolAux:: Polynomial -> Var -> Polynomial
+derivePol:: Polynomial -> Var -> Polynomial
+
+
+countEqualMon:: Monomial -> Polynomial -> Int
+removeEqualMon:: Monomial -> Polynomial -> Polynomial
+
+normVars:: [(Var,Exponent)] -> [(Var,Exponent)]
+removeExpZero:: Polynomial -> Polynomial
+removeCoefZero:: Polynomial -> Polynomial
+sumExponentEqualVars:: Var ->  [(Var,Exponent)] -> Exponent
+removeEqualVars:: Var -> [(Var,Exponent)] -> [(Var,Exponent)]
 
 seperator = '?'
 
@@ -72,4 +90,78 @@ polToStr (x:xs) = monToStr x ++  polToStr xs
                   
 
 
-    
+
+countEqualMon (coef1,vars1) [] = 0
+countEqualMon (coef1,vars1) ((coef2,vars2):xs) | vars1==vars2 = coef2 + countEqualMon (coef1,vars1) xs
+                                               | otherwise = countEqualMon (coef1,vars1) xs
+
+removeEqualMon mon [] = []
+removeEqualMon (coef1,vars1) ((coef2,vars2):xs) | vars1==vars2 = removeEqualMon (coef1,vars1) xs
+                                                | otherwise = (coef2,vars2):removeEqualMon (coef1,vars1) xs
+
+normPol [] = []
+normPol ((coef,vars):xs) | coef==0 = normPol xs
+                         | otherwise = (coef+countEqualMon (coef,vars) xs,vars):normPol (removeEqualMon (coef,vars) xs) 
+
+sumPol p1 p2 = removeCoefZero (normPol (p1++p2))
+
+subPol p1 p2 = removeCoefZero (normPol (p1++map (\(coef,vars) -> (-coef,vars)) p2))
+
+mulPol p1 p2 = removeExpZero (normPol [mulMon m1 m2 | m1<-p1 ,m2<-p2])
+
+sumExponentEqualVars var1 xs = sum [exponent2|(var2,exponent2)<-xs,var1==var2]
+
+removeEqualVars var1 xs = [(var2,exponent2)|(var2,exponent2)<-xs,var1/=var2]
+
+normVars [] = []
+normVars ((var,exp):restVars) = (var,exp+(sumExponentEqualVars var restVars)):normVars(removeEqualVars var restVars)
+
+
+mulMon (coef1,vars1) (coef2,vars2) = (coef1*coef2,normVars (vars1++vars2))
+
+derivePolAux [] _ = []
+derivePolAux ((coef,vars):xs) var = if exp==0 then derivePolAux xs var
+                                 else (coef*exp,varsAfterDer):derivePolAux xs var 
+                                 where    exp = (sumExponentEqualVars var vars)  -- actually just gets the exponent of the var, pol is normalized
+                                          varsAfterDer = (var,exp-1):(filter (\(v,e) -> v/=var) vars)
+
+
+derivePol xs var = removeExpZero (derivePolAux xs var)
+
+
+removeExpZero p = [(coef, [(var,exp)|(var,exp)<-vars,exp/=0]) | (coef,vars) <- p]
+removeCoefZero p = [(coef,vars)|(coef,vars)<-p,coef/=0]
+
+
+main :: IO ()
+main = do
+            putStrLn "Choose an option: "
+            putStrLn "1. Add polynomials"
+            putStrLn "2. Subtract polynomials"
+            putStrLn "3. Multiply polynomials"
+            putStrLn "4. Derive polynomials"
+            str <- getLine
+            let option = read str
+            putStrLn "Insert a polynomial:"
+            pol1str <- getLine
+            let pol1 = parsePol pol1str 
+            if(option==4) then do
+                  putStrLn "Insert the variable to derive by"
+                  varStr <- getLine
+                  let var = read varStr
+                  putStrLn (polToStr (derivePol pol1 var))
+            else do
+                  putStrLn "Insert the second polynomial:"
+                  pol2str <- getLine
+                  let pol2 = parsePol pol2str 
+                  if (option==1) then do
+                        putStrLn "The sum is:"
+                        putStrLn (polToStr (sumPol pol1 pol2))
+                  else if (option==2) then do
+                        putStrLn "The difference is:"
+                        putStrLn (polToStr (subPol pol1 pol2))
+                  else if (option==3) then do
+                        putStrLn "The product is:"
+                        putStrLn (polToStr (mulPol pol1 pol2))
+                  else do
+                        putStrLn "Invalid option"
